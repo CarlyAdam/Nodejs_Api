f79cd8f19dc7fb7d69399f88ee968034fda1f480
Carlos Adan <carlos.adan_ext@scrm.lidl>
2022-01-21 18:12:23 +0100

    product update

--- validations/bookValidation.js
+++ validations/bookValidation.js
@@ -2,14 +2,15 @@ const Joi = require("joi");
 
 //function to validate course
 var validateBook = function validateBook(book) {
-  const schema = {
+  const schema = Joi.object({
     name: Joi.string()
       .min(3)
       .required(),
+      
     price: Joi.string()
     .min(2)
     .required(),
-  };
+  });
 
   return Joi.validate(book, schema);
 };
